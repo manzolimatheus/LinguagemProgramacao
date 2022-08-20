9758fc586b366c80df40c575aad276006077272c
Matheus Manzoli <manzoli.contato@gmail.com>
2022-08-20 12:32:28 -0300

    Create numberGuesser.c

---
+++ aula02/numberGuesser.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_NUMBER 1000
+#define GUESSES 10
+
+void checkIfNumberIsCloseToRandomNumber(int * guess, int * number){
+
+    if (*guess > *number){
+        printf("\n[ O numero sorteado e menor! ]");
+    }else if (*guess < *number){
+        printf("\n[ O numero sorteado e maior! ]");
+    }else{
+        printf("\n[ *** Parabens, voce acertou! *** ]");
+    }
+}
+
+void numberGuesser(){
+    srand(time(NULL));
+
+    int randomNumber = rand() % MAX_NUMBER;
+    int guess;
+
+    //printf("\nNumero gerado: %i", randomNumber);
+    printf("\n[ *** VOCE TEM 5 CHANCES! *** ]");
+
+    for(int i = GUESSES; i > 0; i--){
+        printf("\n[ CHANCES RESTANTES : [%i] ]", i);
+        printf("\n[ Qual seu palpite? ]");
+        printf("\n> ");
+        scanf("%i", &guess);
+        checkIfNumberIsCloseToRandomNumber(&guess, &randomNumber);
+        if (guess == randomNumber){break;}
+    }
+
+    printf("\n[ O jogo acabou! ]");
+
+}
+
+int main() {
+    numberGuesser();
+
+    return 0;
+}
